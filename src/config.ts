6c61ef6b41ad56d20533142530c1ee080d44b5ef
succie <exactf1ash503@gmail.com>
2019-05-26 21:58:08 +0900

    refactor

---
+++ src/config.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from "fs";
+import path from "path";
+
+namespace Config {
+  export interface Token {
+    bot: string;
+    user: string;
+  }
+
+  export interface ChannelSetting {
+    name: string;
+    maxMessageNumber?: number;
+    message?: MessageSetting;
+  }
+
+  export interface DefaultSetting {
+    maxMessageNumber: number;
+    message?: MessageSetting;
+  }
+
+  export interface MessageSetting {
+    start?: string[];
+    running?: string[];
+    end?: string[];
+  }
+
+  export interface IConfig {
+    token: Token;
+    channels: ChannelSetting[];
+    default: DefaultSetting;
+  }
+
+  export function load(): IConfig {
+    const configFilePath = path.join(process.cwd(), "config/default.json");
+    try {
+      const config = readFileSync(configFilePath, "utf-8");
+      return JSON.parse(config);
+    } catch (e) {
+      throw new Error(`Config file ${configFilePath} cannot be read`);
+    }
+  }
+}
+
+export default Config;
--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,54 @@
-import { WebClient } from "@slack/web-api";
 import { map } from "lodash";
-import config from "config";
-
-const bot = new WebClient(config.get("token.bot"));
-const user = new WebClient(config.get("token.user"));
-
-const MAX_MESSAGE_NUMBER = Number(config.get("default.maxMessageNumber"));
-
-interface JointedChannel {
-  id: string;
-  name: string;
-  created: string;
-}
-
-interface ChannelsSetting {
-  name: string;
-  maxMessageNumber: number;
-}
+import Config from "./config";
+import Slack from "./slack";
 
 async function main() {
-  // Bot が参加しているチャンネル一覧.
-  const joinedChannels: JointedChannel[] = await bot.channels
-    .list()
-    .then(res => {
-      return (res.channels as any[])
-        .filter(channel => {
-          return !!channel.is_member;
-        })
-        .map(channel => {
-          return {
-            id: channel.id,
-            name: channel.name,
-            created: channel.created
-          };
-        });
-    })
-    .catch(err => {
-      console.error(err);
-      return [];
-    });
+  const config = Config.load();
+
+  const slack = new Slack.API(config.token.bot, config.token.user);
+
+  const joinedChannels = await slack
+    .channels()
+    .then(channels => channels.filter(channel => channel.is_member));
 
   console.log(
-    `Joined channels of cleaner is ${[...map(joinedChannels, "name")]}`
+    `Joined channels of cleaner is ${map(joinedChannels, "name").join(", ")}`
   );
 
-  const channelSettings = config.get<ChannelsSetting[]>("channels");
+  await Promise.all(
+    Object.entries(joinedChannels).map(async ([_, channel]) => {
+      const messages = await slack.messages(channel, { asc: true });
 
-  for await (let channel of joinedChannels) {
-    const messages = await user.channels
-      .history({
-        channel: channel.id,
-        count: 1000,
-        // チャンネル作成時間がそのチャンネルの一番古い時間のため oldest に設定.
-        oldest: channel.created
-      })
-      .then(res => {
-        // 新しい順で返ってくるので, 古い順にする.
-        return (res.messages as any[]).reverse();
-      })
-      .catch(err => {
-        console.error(err);
-        return [];
-      });
+      console.log(`${channel.name} has ${messages.length} messages.`);
 
-    console.log(`${channel.name} has ${messages.length} messages.`);
-
-    // チャンネル設定を探す.
-    const thisChannelSetting = channelSettings.find(
-      setting => setting.name === channel.name
-    );
-    // あった場合はその maxMessageNumber を設定し, 無ければ MAX_MESSAGE_NUMBER を設定する.
-    const maxMessageNumber = thisChannelSetting
-      ? thisChannelSetting.maxMessageNumber
-      : MAX_MESSAGE_NUMBER;
-
-    // maxMessageNumber を超えていた場合, メッセージ数 - maxMessageNumber 分のメッセージを消す.
-    if (messages.length > maxMessageNumber) {
-      console.log(
-        `${channel.name} exceeds ${messages.length -
-          maxMessageNumber} messages.`
+      const thisChannelSetting = config.channels.find(
+        setting => setting.name === channel.name
       );
-      for (let i = 0; i < messages.length - maxMessageNumber; i++) {
-        await user.chat
-          .delete({ channel: channel.id, ts: messages[i].ts })
-          .catch(err => {
-            console.error(err);
-          });
+
+      const maxMessageNumber =
+        thisChannelSetting && thisChannelSetting.maxMessageNumber !== undefined
+          ? thisChannelSetting.maxMessageNumber
+          : config.default.maxMessageNumber;
+
+      if (messages.length > maxMessageNumber) {
+        console.log(
+          `${channel.name} exceeds ${messages.length -
+            maxMessageNumber} messages.`
+        );
+
+        const deleteMessages = messages.slice(
+          0,
+          messages.length - maxMessageNumber
+        );
+
+        await Promise.all(
+          deleteMessages.map(async message => {
+            await slack.deleteMessage(channel, message);
+          })
+        );
       }
-    }
-  }
+    })
+  );
 }
 
 if (process.env.LOCAL) {
